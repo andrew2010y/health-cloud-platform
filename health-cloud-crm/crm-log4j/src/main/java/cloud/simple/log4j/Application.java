a76a8cd52097d0d65f529a3c7e17a40b64d89a4f
yanxingkui <mengdiege@126.com>
2016-09-29 16:17:49 +0800

    1

---
+++ health-cloud-crm/crm-log4j/src/main/java/cloud/simple/log4j/Application.java
@@ -0,0 +1,16 @@
+package cloud.simple.log4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application  {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ health-cloud-crm/crm-log4j/src/main/java/com/nant/crm/log/AOP.java
@@ -0,0 +1,43 @@
+package com.nant.crm.log;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Configuration
+public class AOP {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Pointcut("execution(public * com.nant.crm.web.controller..*.*(..))")
+	public void webLog() {}
+
+	@Before("webLog()")
+	public void doBefore(JoinPoint joinPoint) {
+		//获取参数的值
+		Object obj [] = joinPoint.getArgs();
+		for(Object o : obj){
+			logger.info(" joinPoint.getArgs() " + o);
+		}
+		logger.info(" Controller before ");
+	}
+
+	@AfterReturning("webLog()")
+	public void doAfterReturning(JoinPoint joinPoint) {
+		Object obj [] = joinPoint.getArgs();
+		Object target = joinPoint.getTarget();
+		logger.info(" joinPoint.getSignature().getName() " +joinPoint.getSignature().getName());
+		logger.info(" joinPoint.getTarget() "+target);
+		for(Object o : obj){
+			System.out.println(o);
+		}
+		logger.info("Controller.doAfterReturning()");
+	}
+}
